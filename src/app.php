4909ffc54fa7a6ba648796fe11e80aad1d38a08a
theredfoxfire <vizzlearn@gmail.com>
2014-10-20 20:31:55 +0700

    5th-deploy

--- src/app.php
+++ src/app.php
@@ -12,16 +12,20 @@ function is_leap_year($year = null){
 	return 0 == $year %400 || (0 == $year % 4 && 0 != $year % 100);
 }
 
+class LeapYearController{
+	function indexAction($year = 2001){
+		if (is_leap_year($year)){
+			return new Response('Yep '.$year.', this is a leap year!');
+		}
+		
+		return new Response('Nope '.$year.', this is not a leap year!');
+	}
+}
+
 $routes = new Routing\RouteCollection();
 $routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
 	'year' => null,
-	'_controller' => function ($request){
-		if (is_leap_year($request->attributes->get('year'))){
-			return new Response('Yep, this is a leap year!');
-		}
-		
-		return new Response('Nope, this is not a leap year!');
-	}
+	'_controller' => 'LeapYearController::indexAction',
 )));
  
 return $routes;
